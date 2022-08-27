3ce207d8e7b8272bde6d472156ab0faf34c3e0f0
Nicholas CH <90885800+2072008NicholasCH@users.noreply.github.com>
2022-08-27 14:07:00 +0700

    categoryDao

--- repo/PT_TugasAkhir_Agenda/src/main/java/com/example/pt_tugasakhir_agenda/dao/CategoryDao.java
+++ repo/PT_TugasAkhir_Agenda/src/main/java/com/example/pt_tugasakhir_agenda/dao/CategoryDao.java
@@ -1,12 +1,37 @@
 package com.example.pt_tugasakhir_agenda.dao;
 
 import com.example.pt_tugasakhir_agenda.model.Category;
+import com.example.pt_tugasakhir_agenda.utility.MyConnection;
 import javafx.collections.ObservableList;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class CategoryDao implements DaoInterface<Category>{
     @Override
     public ObservableList<Category> getData() {
-        return null;
+        ObservableList<Category> catList;
+        catList = FXCollections.observableArrayList();
+        Connection conn = MyConnection.getConnection();
+        String query = "SELECT * FROM Category;";
+        PreparedStatement ps;
+        try {
+            ps = conn.prepareStatement(query);
+            ResultSet result = ps.executeQuery();
+            while (result.next()) {
+                int id = result.getInt("idCategory");
+                String kategori = result.getString("categoryName");
+                Category c = new Category(id, kategori);
+                catList.add(c);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return catList;
     }
 
     @Override

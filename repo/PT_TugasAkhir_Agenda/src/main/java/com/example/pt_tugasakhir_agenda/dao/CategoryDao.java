6763168e9c28170624e5565efb8531cd2e751268
Nicholas CH <90885800+2072008NicholasCH@users.noreply.github.com>
2022-08-27 15:41:04 +0700

    categoryDao

--- repo/PT_TugasAkhir_Agenda/src/main/java/com/example/pt_tugasakhir_agenda/dao/CategoryDao.java
+++ repo/PT_TugasAkhir_Agenda/src/main/java/com/example/pt_tugasakhir_agenda/dao/CategoryDao.java
@@ -12,11 +12,12 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class CategoryDao implements DaoInterface<Category>{
+    private ObservableList<Category> catList;
+    private Connection conn;
     @Override
     public ObservableList<Category> getData() {
-        ObservableList<Category> catList;
         catList = FXCollections.observableArrayList();
-        Connection conn = MyConnection.getConnection();
+        conn = MyConnection.getConnection();
         String query = "SELECT * FROM Category;";
         PreparedStatement ps;
         try {
@@ -36,16 +37,63 @@ public class CategoryDao implements DaoInterface<Category>{
 
     @Override
     public int addData(Category data) {
-        return 0;
+        conn = MyConnection.getConnection();
+        String query = "INSERT INTO Category(idCategory, categoryName) VALUES (?, ?);";
+        PreparedStatement ps;
+        int result;
+        try {
+            ps = conn.prepareStatement(query);
+            ps.setInt(1, data.getIdcategory());
+            ps.setString(2, data.getCategoryname());
+            result = ps.executeUpdate();
+            if (result > 0) {
+                System.out.println("add item successfully");
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return result;
     }
 
     @Override
     public int updateData(Category data) {
-        return 0;
+        conn = MyConnection.getConnection();
+        String query = "UPDATE Category SET categoryName = ? WHERE idCategory = ?;";
+        PreparedStatement ps;
+        int result;
+        try {
+            ps = conn.prepareStatement(query);
+            ps.setString(1, data.getCategoryname());
+            ps.setInt(2, data.getIdcategory());
+            result = ps.executeUpdate();
+            if (result > 0) {
+                System.out.println("update item successfully");
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return result;
     }
 
     @Override
     public int deleteData(Category data) {
-        return 0;
+        conn = MyConnection.getConnection();
+        String query = "DELETE FROM Category WHERE idCategory = ?;";
+        PreparedStatement ps;
+        int result;
+        try {
+            ps = conn.prepareStatement(query);
+            ps.setInt(1, data.getIdcategory());
+            result = ps.executeUpdate();
+            if (result > 0) {
+                System.out.println("delete item successfully");
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return result;
     }
 }
